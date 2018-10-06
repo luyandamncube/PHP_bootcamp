3f5dc84384f3aa633d8bc34f9cf2332536ee228f
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-10-06 16:19:31 +0200

    Added rush work

---
+++ rush00/index.php
@@ -0,0 +1,39 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Luxury</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css"/>
+</head>
+<body>
+    <div class="container-1" > 
+            <a href="#"><img class="logo" src="img/logo.png" alt="logo"></a>
+            <ul class="ul">
+                <li>
+                <a class="a" href="#">Dropdown Link</a>
+                <ul class="ul">
+                    <li class="li"><a href="#">Link 1</a></li>
+                    <li class="li"><a href="#">Link 2</a></li>
+                    <li class="li"><a href="#">Link 3</a></li>
+                </ul>
+                </li>
+            </ul>
+            <a class="a" href="#"> MENS </a>
+            <a class="a" href="#"> WOMENS </a>
+            <a class="a" href="#"> KIDS </a>
+            <a href="#"><img class="logo" src="img/basket.jpg" alt="basket"></a>
+    </div>
+    <div class="container-2"></div>
+        <div> 2 </div>
+        <div> 3 </div>
+        <div> 4 </div>
+        <div class="footer"> 
+            © footer 2018
+        </div>
+    </div>
+</body>
+</html>
