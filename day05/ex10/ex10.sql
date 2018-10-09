610c15ca605737e74e692fd96cec61572831ac22
luyandamncube <59448873@mylife.unisa.ac.za>
2018-10-09 16:47:11 +0200

    comment

--- day05/ex10/ex10.sql
+++ day05/ex10/ex10.sql
@@ -1,4 +1,5 @@
 --JOIN combine rows from two or more tables on a related column
+--caeful to match the source column to target correctly
 SELECT title AS 'Title', summary AS 'Summary', prod_year AS 'Production Year'
   FROM film
   INNER JOIN genre ON genre.id_genre = film.id_genre
